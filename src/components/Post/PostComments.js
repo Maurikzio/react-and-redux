cb6ca8ce4b713990970dfe535a483fa0d6774c03
Maurikzio <m.morocho1196@gmail.com>
2020-08-01 19:42:48 +0300

    [fix] no comments message for new posts

--- src/components/Post/PostComments.js
+++ src/components/Post/PostComments.js
@@ -8,15 +8,19 @@ const PostComments = ({comments}) => {
             <h3>Comments:</h3>
             <ul className='postComments'>
                 {
-                    comments.map((comment) => (
-                        <li key={comment.id}>
-                            <p><strong>Post id:</strong> {comment.postId}</p>
-                            <p><strong>Comment id:</strong> {comment.id}</p>
-                            <p><strong>Name:</strong> {comment.name}</p>
-                            <p><strong>Email:</strong> {comment.email}</p>
-                            <p><strong>Body:</strong> {comment.body}</p>
-                        </li>
-                    ))
+                    comments.length === 0 ? (
+                        <p>No comments yet...</p>
+                    ) : ( 
+                        comments.map((comment) => (
+                            <li key={comment.id}>
+                                <p><strong>Post id:</strong> {comment.postId}</p>
+                                <p><strong>Comment id:</strong> {comment.id}</p>
+                                <p><strong>Name:</strong> {comment.name}</p>
+                                <p><strong>Email:</strong> {comment.email}</p>
+                                <p><strong>Body:</strong> {comment.body}</p>
+                            </li>
+                        ))
+                    )
                 }
             </ul>
             </div>
